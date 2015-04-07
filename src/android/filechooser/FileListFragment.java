25d0c36365c4605ce2181854e966d903190a914b
shayk89 <shaykemp89@gmail.com>
2015-04-06 20:31:21 -0700

    Update FileListFragment.java

--- src/android/filechooser/FileListFragment.java
+++ src/android/filechooser/FileListFragment.java
@@ -16,7 +16,7 @@
 
 package com.ipaulpro.afilechooser;
 
-import com.asternotes.thriftkarma.R
+import com.asternotes.thriftkarma.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
