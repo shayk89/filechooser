990c380e1ade92bd55372a13ff5f005f6a37db23
shayk89 <shaykemp89@gmail.com>
2015-04-06 21:25:58 -0700

    Update FileListAdapter.java

--- src/android/filechooser/FileListAdapter.java
+++ src/android/filechooser/FileListAdapter.java
@@ -16,7 +16,7 @@
 
 package com.ipaulpro.afilechooser;
 
-import com.asternotes.thriftkarma.R
+import com.checklater.asternotes.R
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
